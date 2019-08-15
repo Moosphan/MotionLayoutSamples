b6bad25a87ffbc64e4fd0d859b129c219c0f91be
suhao <2301230182@qq.com>
2019-08-15 19:21:48 +0800

    add multi views motion sample and update codes

---
+++ app/src/main/java/com/moos/constraint/practice/MultiBallActivity.kt
@@ -0,0 +1,13 @@
+package com.moos.constraint.practice
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.moos.constraint.R
+
+class MultiBallActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_multi_ball_layout)
+    }
+}

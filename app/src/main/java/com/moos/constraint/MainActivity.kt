7c7f39b620e6aac558909028424bffc6764bb971
moosphan <moosphon@gmail.com>
2020-08-16 16:38:49 +0800

    make a brief anim wihout motion

--- app/src/main/java/com/moos/constraint/MainActivity.kt
+++ app/src/main/java/com/moos/constraint/MainActivity.kt
@@ -1,13 +1,39 @@
 package com.moos.constraint
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.moos.constraint.practice.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    class NoScrollLinearLayoutManager(context: Context?) : LinearLayoutManager(context) {
+        private var scrollable = true
+
+        fun enableScrolling() {
+            scrollable = true
+        }
+
+        fun disableScrolling() {
+            scrollable = false
+        }
+
+        override fun canScrollVertically(): Boolean {
+            val canScroll = super.canScrollVertically() && scrollable
+            Log.e("MainActivity", "当前是否能纵向滑动：$canScroll")
+            return canScroll
+        }
+
+
+
+        override fun canScrollHorizontally() =
+            super.canScrollVertically()
+
+                    && scrollable
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,10 +42,22 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun init() {
-        recyclerView.layoutManager =
-            androidx.recyclerview.widget.LinearLayoutManager(this)
-        val adapter = EntranceListAdapter()
+        recyclerView.layoutManager = NoScrollLinearLayoutManager(this)
+
+        val adapter = EntranceListAdapter(this)
         recyclerView.adapter = adapter
+        adapter.setOnSlideCallback {
+            if (it) {
+                (recyclerView.layoutManager as NoScrollLinearLayoutManager).enableScrolling()
+                adapter.hideDialog()
+            }
+            else
+                (recyclerView.layoutManager as NoScrollLinearLayoutManager).disableScrolling()
+        }
+
+        adapter.setOnProgressListener {
+            adapter.showDialog("进度为：$it")
+        }
         adapter.setOnEntranceSelectListener {
             when(it) {
                 0 -> startActivity(Intent(this, MotionSampleActivity::class.java))
@@ -27,7 +65,17 @@ class MainActivity : AppCompatActivity() {
                 2 -> startActivity(Intent(this, AppBarMotionActivity::class.java))
                 3 -> startActivity(Intent(this, DrawerMotionActivity::class.java))
                 4 -> startActivity(Intent(this, LottieMotionActivity::class.java))
-                5 -> startActivity(Intent(this, DetailActivity::class.java))
+                5 -> {
+                    val intent = Intent(this, DetailActivity::class.java)
+                    intent.putExtra(DetailActivity.LAYOUT_ID_KEY, R.layout.scene_search_anim)
+                    startActivity(intent)
+                }
+                6 -> startActivity(Intent(this, UserGuideActivity::class.java))
+                7 -> {
+                    val intent = Intent(this, DetailActivity::class.java)
+                    intent.putExtra(DetailActivity.LAYOUT_ID_KEY, R.layout.scene_key_cycle_sample)
+                    startActivity(intent)
+                }
             }
         }
     }
